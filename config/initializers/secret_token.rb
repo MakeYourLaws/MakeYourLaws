6b7c44d958f66d118029882ad71a33f2503b82a9
Sai <github@saizai.com>
2011-11-18 09:27:47 -0500

    new rails project w/ old html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeyourlawsOrg::Application.config.secret_token = 'ef878d8ce6256049596122eeef3dd779604c8fe2158c9d59d354a6b8bda70f4e3c8b28d29a335d6c38ea768928077bb91c3cebbc7a4883474958912e948b832b'
