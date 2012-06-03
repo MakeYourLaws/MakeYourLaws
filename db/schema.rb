a23dbd94e26669a75e86178a50b10ca5484cdfd5
Sai <github@saizai.com>
2012-06-03 08:52:42 -0400

    update for devise 2.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423095705) do
+ActiveRecord::Schema.define(:version => 20120423131431) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(:version => 20120423095705) do
     t.string   "secret"
     t.text     "raw_info"
     t.integer  "lock_version", :default => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
     t.string   "url"
   end
 
@@ -67,20 +67,20 @@ ActiveRecord::Schema.define(:version => 20120423095705) do
     t.string   "session_id",                  :null => false
     t.text     "data"
     t.integer  "lock_version", :default => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "",  :null => false
+    t.string   "encrypted_password",     :default => "",  :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -89,21 +89,26 @@ ActiveRecord::Schema.define(:version => 20120423095705) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",                       :default => 0
+    t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
-    t.string   "name",                                                  :null => false
-    t.string   "login",                                                 :null => false
-    t.integer  "lock_version",                          :default => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.string   "name",                                    :null => false
+    t.string   "login",                                   :null => false
+    t.integer  "lock_version",           :default => 0
     t.string   "unconfirmed_email"
+    t.string   "gauth_secret"
+    t.string   "gauth_enabled",          :default => "f"
+    t.string   "gauth_tmp"
+    t.datetime "gauth_tmp_datetime"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["gauth_tmp"], :name => "index_users_on_gauth_tmp"
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
   add_index "users", ["name"], :name => "index_users_on_name"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
