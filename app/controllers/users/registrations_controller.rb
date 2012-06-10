ba05391dac6df8b785123642b1fe54969f27fe92
Sai <github@saizai.com>
2012-06-09 20:33:16 -0400

    Fix login & signup errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,32 +12,31 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   def create
-     build_resource
-     
-     if session["devise.identity"] and identity = Identity.find(session["devise.identity"])
-       resource.identities << identity
-     end
-     
-     if resource.save
-       if resource.active_for_authentication?
-         set_flash_message :notice, :signed_up if is_navigational_format?
-         sign_in(resource_name, resource)
-         respond_with resource, :location => after_sign_up_path_for(resource)
-       else
-         set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
-         expire_session_data_after_sign_in!
-         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+    build_resource
+
+    if session["devise.identity"] and identity = Identity.find(session["devise.identity"])
+      resource.identities << identity
+    end
+    
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with_navigational(resource) do
+         if session["devise.identity"]
+           render :new_from_id
+         else
+           render :new 
+         end
        end
-     else
-       clean_up_passwords(resource)
-       respond_with_navigational(resource) do
-          if session["devise.identity"]
-            render :new_from_id
-          else
-            render :new 
-          end
-        end
-     end
-   end
-   
+    end
+  end
 end
