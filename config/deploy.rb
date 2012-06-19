a1a4dec77c7d998987fe2cc303194879e62abddd
Sai <github@saizai.com>
2012-06-19 08:46:55 -0400

    rm superdeploy plugin, add shared data directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,33 +49,61 @@ server '173.255.252.140', :app, :db, :web, :primary => true # We have no access 
 # load 'deploy/assets' # executed by default as deploy:assets:precompile
 
 # Choose your default deploy methods
-namespace (:deploy) do
+namespace :deploy do
   task :restart, :roles => :app do
-    # deploy.mongrel.seesaw
-    # deploy.god.restart
     deploy.passenger.restart
   end
   
   # Use a shared config directory. Run cap deploy:configs:setup first.
   after "deploy:finalize_update", "deploy:configs:symlink"
-  # after "deploy:update_code", "deploy:files:symlink"
+  after "deploy:finalize_update", "deploy:files:symlink"
   # after "deploy:restart", "deploy:restart_mail_fetcher"
   
   task :restart_mail_fetcher, :roles => :app do
     run "cd #{release_path} && RAILS_ENV=#{fetch(:rails_env, "production")} script/mail_fetcher restart"
   end
   
-#  after "deploy:update_code", "deploy:install_gems"
-  task :install_gems, :roles => :app do
-    cmd = "cd #{release_path} && RAILS_ENV=#{fetch(:rails_env, "production")} rake gems:install"
-    use_sudo ? sudo(cmd) : run(cmd)
+  namespace :passenger do
+    desc "Restart using Passenger" 
+    task :restart, :roles => :app do
+      run "touch #{current_path}/tmp/restart.txt" 
+    end
   end
   
-  # Set up special permissions
-#  after "deploy:update_code", "deploy:set_permissions_staging"
-#  task :set_permissions_staging, :except => { :no_release => true } do 
-#      run "rm -f #{release_path}/config/database.yml"
-#      run "cp #{deploy_to}/shared/config/database.yml #{release_path}/config"
-#      run "chmod 775 #{release_path}/config/database.yml"
-#  end  
+  namespace :files do
+    desc "Creates symlinked shared public-files and private-data folders in shared folder"
+    task :symlink, :roles => :app do
+      run "rm -rf #{release_path}/public/files"
+      run "mkdir -p #{shared_path}/public_files"
+      run "chmod 777 #{shared_path}/public_files"
+      run "ln -nfs #{shared_path}/public_files #{release_path}/public/files"
+      
+      run "rm -rf #{release_path}/db/data"
+      run "mkdir -p #{shared_path}/data"
+      run "chmod 770 #{shared_path}/data"
+      run "ln -nfs #{shared_path}/data #{release_path}/db/data"
+    end
+  end
+  
+  namespace :config do
+    desc "Override config files w/ whatever's in the shared/config path (e.g. passwords, api keys)"
+    task :symlink, :roles => :app do         
+      # Be extra careful about exposing these
+      run "chmod -R go-rwx #{shared_path}/config"
+    
+      # For all files in the shared config path, symlink in the shared config
+      # For some reason, this Dir actually runs on the *local* system rather than the remote. Lame.
+      #    Dir[File.join(shared_path, 'config', '**', '*.rb')].each do |c|
+      # So here's a hack w/ find to do it the ugly way :(
+      config_files = ''
+      # Find all regular files (not directories) in the shared config path
+      run("find #{shared_path}/config -type f") do |channel, stream, data| 
+       config_files << data
+      end
+      # Extract the names of all config files
+      config_files.strip.split("\n").map{|f| f.sub("#{shared_path}/config/", '').strip}.each do |c|
+        run "ln -sf #{shared_path}/config/#{c} #{release_path}/config/#{c}" # And symlink in the server's version, overwriting (-f) whatever was there
+      end
+    end
+  end
 end
