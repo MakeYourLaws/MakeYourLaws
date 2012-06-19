11050c4173a25dc33bb9115476f065b20cf889ba
Sai <github@saizai.com>
2012-06-19 06:27:45 -0400

    add mixpanel tracking

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,19 +1,25 @@
 class MainController < ApplicationController
   def index
+    track! 'page load', :page => 'index'
   end
 
   def contact
+    track! 'page load', :page => 'contact'
   end
 
   def principles
+    track! 'page load', :page => 'principles'
   end
 
   def introduction
+    track! 'page load', :page => 'introduction'
   end
 
   def strategy
+    track! 'page load', :page => 'strategy'
   end
   
   def help
+    track! 'page load', :page => 'help'
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,8 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       resource = build_resource({:email => identity.email, :name => identity.name,
                                   :login => identity.nickname})
       resource.identities << identity
+      
+      track! 'signup from id', :provider => identity.provider
       respond_with_navigational(resource){ render :new_from_id }
     else
+      track! 'signup from id error'
       flash[:error] = "Something's wrong with your session. Please try again and ensure cookies are on."
       redirect_to signup_path
     end
@@ -20,15 +23,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     
     if resource.save
       if resource.active_for_authentication?
+        track! "signup completed"
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
+        track! "signup pending", :issue => resource.inactive_message
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
+      track! 'signup started'
       clean_up_passwords resource
       respond_with_navigational(resource) do
          if session["devise.identity"]
