7cf94541c37c402a848a1db7579d0e3199b7de51
Sai <github@saizai.com>
2012-09-22 17:49:41 -0700

    fix submodel loading problem

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module MakeyourlawsOrg
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    # config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
     
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
