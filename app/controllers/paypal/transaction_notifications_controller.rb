d2972234638348a55b6e0c3651502ba57f87aa6c
Sai <github@saizai.com>
2012-09-22 19:45:46 -0700

    fix pdt

--- app/controllers/paypal/transaction_notifications_controller.rb
+++ app/controllers/paypal/transaction_notifications_controller.rb
@@ -6,7 +6,7 @@ class Paypal::TransactionNotificationsController < ApplicationController
   def create
     notify = ActiveMerchant::Billing::Integrations::PaypalAdaptivePayment::Notification.new(request.raw_post)
     
-    @subtransaction = Paypal::Transaction.find_by_paypal_transaction_id notify.params['txn_id']
+    @subtransaction = Paypal::Subtransaction.find_by_paypal_transaction_id notify.params['txn_id']
     @transaction_notification = Paypal::TransactionNotification.new
     @transaction_notification.details_json = notify.params.to_json
     # e.g. {"transaction_subject":"","payment_date":"18:17:32 Sep 22, 2012 PDT","txn_type":"web_accept","last_name":"'","residence_country":"US","item_name":"","payment_gross":"1.00","mc_currency":"USD","business":"c4@makeyourlaws.org","payment_type":"instant","protection_eligibility":"Ineligible","verify_sign":"Ap7A90.xtS8ZY0PiomkCG1dgf9S3A7JBMDRpbIh2W2wFY24itIdwQdrs","payer_status":"verified","tax":"0.00","payer_email":"pp@saizai.com","txn_id":"8CF8425087487523J","quantity":"0","receiver_email":"c4@makeyourlaws.org","first_name":"Sai","payer_id":"J64ZRFCZ2A4MC","receiver_id":"R9QPE87RRNTH6","item_number":"","payment_status":"Completed","payment_fee":"0.33","mc_fee":"0.33","mc_gross":"1.00","custom":"","charset":"windows-1252","notify_version":"3.7","ipn_track_id":"e03a84c4457c0"}
