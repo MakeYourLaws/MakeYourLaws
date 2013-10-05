88b3ff64f15b7b577292ab3d79f1e656849f0e92
Sai <github@saizai.com>
2013-10-05 13:35:37 -0700

    don't show providers in still-testing status to public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,6 +48,10 @@ class ApplicationController < ActionController::Base
   end
   helper_method :tor?
   
+  def admin?
+    current_user && (current_user.id == 1)
+  end
+  
   def deny_tor_users
     redirect_to root_path, alert: 'Tor users may not use that functionality.' if tor?
   end
