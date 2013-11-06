943ca00bfe2fa342501e94f19c2e641441928c05
Sai <github@saizai.com>
2013-11-06 14:40:14 -0800

    genericize, turn off tracking for tor users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,37 +1,7 @@
 class MainController < ApplicationController
-  def index
-    track! 'page load', :page => 'index'
-  end
-
-  def contact
-    track! 'page load', :page => 'contact'
-  end
-
-  def principles
-    track! 'page load', :page => 'principles'
-  end
-
-  def introduction
-    track! 'page load', :page => 'introduction'
-  end
-
-  def strategy
-    track! 'page load', :page => 'strategy'
-  end
-  
-  def help
-    track! 'page load', :page => 'help'
-  end
-  
-  def help_legal
-    track! 'page load', :page => 'help_legal'
-  end
-  
-  def tos
-    track! 'page load', :page => 'tos'
-  end
-  
-  def privacy
-    track! 'page load', :page => 'privacy'
+  def method_missing page
+    if !Dir.glob(File.join(Rails.root, 'app', 'views', 'main', "#{page}.*")).blank?
+      track! 'page load', :page => page unless tor?
+    end
   end
 end
