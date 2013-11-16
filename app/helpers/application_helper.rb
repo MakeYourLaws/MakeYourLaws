76c6327fcd606f3df4e249c3e50a9d2f729ab900
Sai <github@saizai.com>
2013-11-15 18:18:13 -0800

    add page titles to meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def m string
      RDiscount.new(string).to_html.html_safe unless string.blank?
   end  
+  
+  def t string
+    content_for :title, string
+    content_tag :h1, string
+  end
 end
