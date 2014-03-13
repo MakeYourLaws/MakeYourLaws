7fcbb408334180c47ea889888390b359695758d7
Sai <github@saizai.com>
2014-03-12 18:43:23 -0700

    add basic caching throughout; move rack-attack to different redis db; make dev env faster when not working on assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def m string
      RDiscount.new(string).to_html.html_safe unless string.blank?
-  end  
-  
+  end
+
+  def tor_s
+    (tor? ? (hidden_service? ? 'tor' : 'hiddenservice') : 'nontor')
+  end
+
   def t string
+    @title = string
     content_for :title, string
     content_tag :h1, string
   end
