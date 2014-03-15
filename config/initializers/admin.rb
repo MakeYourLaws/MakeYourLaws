154192c6002c33379b852e68d53046e4063451d7
Sai <github@saizai.com>
2014-03-15 12:44:07 -0700

    simplify lookup

--- config/initializers/admin.rb
+++ config/initializers/admin.rb
@@ -1,8 +1,6 @@
 # via http://www.simple10.com/resque-admin-in-rails-3-routes-with-cancan/
 class IsAdmin
   def self.matches?(request)
-    current_user = request.env['warden'].user
-    return false if current_user.blank?
-    Ability.new(current_user).is_admin?
+    request.env['warden'].user.try(:is_admin?)
   end
 end
