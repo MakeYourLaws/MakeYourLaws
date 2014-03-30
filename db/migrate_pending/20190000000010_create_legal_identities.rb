546b72851c1321a2373ad619d65d0c110ed452fd
Sai <github@saizai.com>
2014-03-30 11:28:04 -0700

    break out legal ID stuff

--- db/migrate_pending/20190000000010_create_legal_identities.rb
+++ db/migrate_pending/20190000000010_create_legal_identities.rb
@@ -1,77 +1,86 @@
 class CreateLegalIdentities < ActiveRecord::Migration
   def change
     create_table :legal_identities do |t|
+      # Both humans and corporations
+      t.date :birthdate
+      # This is actually 1:M not 1:1 (EIN/SSN/etc can be changed), but for now we're not handling that
+      t.integer :irs_id, :limit => 4 # bytes — EIN for corps, SSN for humans
+      t.integer :party_id # refers to an FEC::Committee ID this entity is formally affiliated with (if any)
+
+      # Human specific
+      t.boolean :us_citizen_or_greencard # US Citizen or permanent resident / green card holder?
+      t.boolean :govt_contractor
+      t.references :employer # a legal_identity. Special cases: self.id (self-employed), nil (unemployed)
+      t.references :occupation
+
+      t.integer :lock_version
+      t.timestamps
+
+      t.index :irs_id, :unique => true # allows nil in most DBs
+      t.index :employer  # for autocomplete
+      t.index :occupation
+    end
+
+    create_table :occupations do |t|
+      t.string :name, :null => false  # special: "unemployed", "retired", "homemaker"
+      t.string :irs_id
+
+      t.index :name
+    end
+
+    create_table :address_usages do |t|
+      t.references :legal_identity
+      t.references :address
+      t.string :type # e.g. residential, mailing, billing, voting, fec
+      t.date :from
+      t.date :to
+
+      t.index [:legal_identity, :type]
+    end
+
+    create_table :addresses do |t|
+      # TODO: Support Google's location info
+
+      # FEC spec
+      t.string :street_address_1, :street_address_1, :limit => 34
+      t.string :city, :limit => 30
+      t.string :state, :limit => 2
+      t.integer :zip, :limit => 4 # bytes
+      t.float :lat, :lng
+
+      t.index [:lat, :lng]
+      t.index [:state, :city, :street_address_1]
+      t.index :full_addresss, :unique => true
+    end
+
+    # Import from FEC/USPS definitions
+    create_table :states do |t|
+      t.string :abbreviation, :limit => 2
+      t.string :name
+
+      t.index :abbreviation, :unique => true
+    end
+
+    create_table :name_usages do |t|
+      t.references :legal_name
+      t.references :legal_identity
+      t.date :from
+      t.date :to
+      t.string :authority # people can have different names according to different government entities
+    end
+
+    create_table :legal_names do |t|
       t.string :full_name
-      t.string :name_prefix, :length => 10
+      t.string :name_prefix, :length => 10 # Based on FEC filing requirements
       t.string :first_name, :length => 50
       t.string :middle_name, :length => 50
       t.string :last_name, :length => 50
       t.string :name_suffix, :length => 10 # Jr., Sr., I, II, etc
-      
-      t.integer :previous_names_count
-      
-      t.text :residence_address
-      t.text :mailing_address
-      
-      t.date :birthdate 
-      t.string :gender, :length => 1 # M, F, O, nil - no free text here, since those aren't legally defined/recognized
-      t.string :party  # full name of party, minus the "Party" part
-      
-      t.string :employer # specials: "self" and nil (for unemployed)
-      t.string :occupation # special: "unemployed", "retired", "homemaker"
-      
-      t.integer :ssn, :limit => 4 # bytes, i.e. max 2147483647
-      
-      t.string :citizenship, :limit => 1 # us (C)itizen, (G)reencard, nil
-                                         # eventually we'll have to support other citizenships too but not yet
-      
-      t.integer :us_house_district
-      t.integer :us_senate_district
-      t.integer :us_congress_district # ?
-      t.string :city, :length => 50
-      t.string :county, :length => 50
-      t.integer :ward
-      t.integer :election_district
-      t.integer :legislative_district
-      t.integer :county_voter_id
-      t.integer :state_voter_id
-      t.string :source 
-      t.date :source_date
-      
-      t.reference :duplicate_of # for misspellings and the like; set on the non-canonical entries
-                                # also set for change of address, set on the stale entries
-                                # could make this HAABTM vs addresses, but given the scales involved it's probably better this way
-      # may need some way to represent couples or other multi-identity pseudorecords; again that might be HAABTM
-      
+
       t.integer :lock_version
       t.timestamps
+
+      t.index [:legal_idenity, :from]
     end
-    
-    # This is not technically true; multiple people with the exact same name can live at the same address.
-    # However, for the purposes of any kind of identification we can easily run, we can't distinguish them.
-    # Therefore, we rule out this case and ensure at least some uniqueness. Might distinguish by SSN eg in the future.
-    add_index :legal_identities, [:full_name, :address], :unique => true
-    add_index :ssn, :unique => true
-    
-    add_index :legal_identities, :employer
-    add_index :legal_identities, :occupation # use for autocomplete
-    add_index :legal_identities, :citizenship
   end
-  
-  create_table :names do |t|
-    t.references :legal_identity
-    
-    t.string :full_name
-    t.string :name_prefix, :length => 10
-    t.string :first_name, :length => 50
-    t.string :middle_name, :length => 50
-    t.string :last_name, :length => 50
-    t.string :name_suffix, :length => 10 # Jr., Sr., I, II, etc
-    
-    t.date :last_used
-    
-    t.integer :lock_version
-    t.timestamps
-  end
-  
 end
--- db/migrate_pending/20190000000040_create_voters.rb
+++ db/migrate_pending/20190000000040_create_voters.rb
@@ -1,5 +1,23 @@
 class CreateVoters < ActiveRecord::Migration
   def change
-  
+    create_table :voting_rights do |t|
+      t.references :legal_identity
+      t.references :district
+      t.datetime :from
+      t.datetime :to
+      t.string :voter_id # gotten from voter registration lists
+
+      t.index [:legal_identity, :district]
+      t.index :voter_id
+    end
+
+    create_table :districts do |t|
+      t.string :level # federal, state, county, city
+      t.string :jurisdiction # eg Arizona, US Federal, Alameda, etc
+      t.string :branch # eg state, house, ward, etc
+      t.string :name # eg 7
+
+      t.index [:jurisdiction, :name]
+    end
   end
 end
