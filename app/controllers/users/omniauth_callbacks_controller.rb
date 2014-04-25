ec0889446a94ec0fb8dff51419bf94b5a1b291b4
Sai <github@saizai.com>
2014-04-24 18:16:24 -0700

    extract coinbase uid

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,14 +5,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       #   flash[:error] = "#{kind} is not a supported identification method."
       #   redirect_to login_path and return
       # end
-    
+
       auth = request.env["omniauth.auth"] # 1. Get the Omniauth response
-    
+
       unless id = Identity.by_omniauth(auth) # 2. Update or create an Identity to go with it
         flash[:error] = I18n.t "devise.omniauth_callbacks.failure", :kind => kind # TODO: add reason
         redirect_to login_path and return
       end
-    
+
       if user_signed_in? # 3a. Add this identity to the logged in user
         @user = current_user
         @user.identities << id
