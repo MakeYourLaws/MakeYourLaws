3665b722092ff2c66b20ad91608b69d3b1afba28
Sai <github@saizai.com>
2014-05-30 17:43:20 -0400

    update earmarking aor

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ MakeyourlawsOrg::Application.routes.draw do
       get 'bitcoin/pacs', action: 'bitcoin_pacs'
       get 'bitcoin/aor', to: redirect('https://docs.google.com/document/d/1AVuXrnNPynEcIq07MTgZPCjXMJICxtKeRH0jshYHVk8/', status: 302)
       get 'earmarks'
-      get 'earmarks/aor', to: redirect('https://docs.google.com/document/d/1aebcD6vyN-B-Ikb923N3Mz3lOvgHYL-06wKgwvozI1Y/', status: 302)
+      get 'earmarks/aor', to: redirect('https://docs.google.com/document/d/1YKv5Csfkyg3LAzfP_pkJsgFn9Gns8gWaKyvNbHFsoDE/', status: 302)
       get 'tplf'
     end
   end
