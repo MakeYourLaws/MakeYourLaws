f99bbbff751e8f5dc1842a5ff2ccc9ddf64bc551
Sai <github@saizai.com>
2014-06-08 13:13:16 -0400

    deprecate login from users for migration to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517213307) do
+ActiveRecord::Schema.define(version: 20140605154549) do
 
   create_table "addresses", force: true do |t|
     t.string  "country",                     default: "United States", null: false
@@ -800,7 +800,7 @@ ActiveRecord::Schema.define(version: 20140517213307) do
     t.datetime "locked_at"
     t.string   "authentication_token"
     t.string   "name",                                null: false
-    t.string   "login",                               null: false
+    t.string   "login"
     t.integer  "lock_version",           default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -810,7 +810,7 @@ ActiveRecord::Schema.define(version: 20140517213307) do
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
+  add_index "users", ["login"], name: "index_users_on_login", using: :btree
   add_index "users", ["name"], name: "index_users_on_name", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unconfirmed_email"], name: "index_users_on_unconfirmed_email", unique: true, using: :btree
