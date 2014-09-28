e513e6afa98432510d5db358700b2ab67727528d
Sai <github@saizai.com>
2014-09-28 15:59:55 -0400

    add mailhopper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ MakeyourlawsOrg::Application.configure do
     Rails.application.routes.default_url_options[:host] = 'www.makeyourlaws.org'
   end
   config.action_mailer.default_url_options = { host: 'www.makeyourlaws.org' }
-  config.action_mailer.delivery_method = :sendmail # :smtp
+  config.action_mailer.delivery_method = :mailhopper
   # defaults to -i -t. However, Mail::Sendmail actually puts recipients on the command line, so
   #  if -t gets them from the headers, it screws things up. Thus, no -t.
   config.action_mailer.sendmail_settings = { arguments: '-i ' }
