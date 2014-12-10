aa15299e4b3540dec9926d817ea9b8921db04222
Sai <github@saizai.com>
2014-12-10 20:59:57 +0000

    fix resque-pool bootstrapping issue

--- config/initializers/rolify.rb
+++ config/initializers/rolify.rb
@@ -1,3 +1,6 @@
+# FIXME: This is a hack to fix environment loading in resque-pool leading to 'constant not found'
+Rails.application.eager_load! unless (defined? Role)
+
 Rolify.configure do |config|
   # Dynamic shortcuts for User class (user.is_admin? like methods). Default is: false
   # Enable this feature _after_ running rake db:migrate as it relies on the roles table
