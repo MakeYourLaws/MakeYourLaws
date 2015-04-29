fca220d36f6055fbdbc18c332d9a426641ddbc1f
Sai <github@saizai.com>
2015-04-29 09:37:58 +0100

    switch to www

--- config/deploy.rb
+++ config/deploy.rb
@@ -170,7 +170,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      execute 'curl -s https://makeyourlaws.org > /dev/null' # Warm up the server
+      execute 'curl -s https://www.makeyourlaws.org > /dev/null' # Warm up the server
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
