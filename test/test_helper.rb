fd5a4ac3e6056d918ece5baf8d04da05d1d294ac
Sai <github@saizai.com>
2015-06-13 07:43:31 -0400

    add coveralls to test_helper too; should deprecate it and move to spec only

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'coveralls'
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
