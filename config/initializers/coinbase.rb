9e025f630828d8c3d8859b18c5d7200e7a108c73
Sai <github@saizai.com>
2015-10-03 11:28:16 -0400

    update coinbase initializer

--- config/initializers/coinbase.rb
+++ config/initializers/coinbase.rb
@@ -2,7 +2,7 @@ require 'coinbase/wallet'
 require 'em-http'
 
 if Rails.env.production?
-  COINBASE = Coinbase::Wallet::AsyncClient.new(api_key: Keys.get('coinbase_key_pac'), api_secret: Keys.get('coinbase_secret_pac')))
+  COINBASE = Coinbase::Wallet::AsyncClient.new(api_key: Keys.get('coinbase_key_pac'), api_secret: Keys.get('coinbase_secret_pac'))
 else
-  COINBASE = Coinbase::Wallet::AsyncClient.new(api_key: Keys.get('coinbase_key_pac'), api_secret: Keys.get('coinbase_secret_pac')), api_url: "https://api.sandbox.coinbase.com")
+  COINBASE = Coinbase::Wallet::AsyncClient.new(api_key: Keys.get('coinbase_key_pac'), api_secret: Keys.get('coinbase_secret_pac'), api_url: "https://api.sandbox.coinbase.com")
 end
